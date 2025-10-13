36aad3356be5b8dd101f29af65aac3633dd41ae8
koufeixiang <koufeixiang@bytedance.com>
2025-10-13 20:47:48 +0800

    fix(*): modify resource quantity unit

--- pkg/util/machine/topology.go
+++ pkg/util/machine/topology.go
@@ -661,7 +661,7 @@ func GetThreadSiblingInfo(machineInfo *info.MachineInfo) map[int]int {
 
 	for _, node := range machineInfo.Topology {
 		for _, core := range node.Cores {
-			klog.Infof("[KFX]GetThreadSiblingInfo node=%v, core=%v, threads=%v", node, core, core.Threads)
+			klog.Infof("[KFX]GetThreadSiblingInfo node=%v, core=%v, threads=%v", node.Id, core.Id, core.Threads)
 			if len(core.Threads) == 2 {
 				threadSiblingMap[core.Threads[0]] = core.Threads[1]
 			}
